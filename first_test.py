b5c9a08197163c91b48f1d8b35ed66c73745d68b
Olasupo Olawale Martin <rytesoft@hotmail.com>
2023-06-13 21:27:32 +0200

    second test

--- first_test.py
+++ first_test.py
@@ -1,5 +1,39 @@
 import pytest
 from PythonAppJenkinsCICD import *
+from pylint import *
+
+
+class Fruit:
+    def __init__(self, name):
+        self.name = name
+        self.cubed = False
+
+    def cube(self):
+        self.cubed = True
+
+
+class FruitSalad:
+    def __init__(self, *fruit_bowl):
+        self.fruit = fruit_bowl
+        self._cube_fruit()
+
+    def _cube_fruit(self):
+        for fruit in self.fruit:
+            fruit.cube()
+
+
+# Arrange
+@pytest.fixture
+def fruit_bowl():
+    return [Fruit("apple"), Fruit("banana")]
+
+
+def test_fruit_salad(fruit_bowl):
+    # Act
+    fruit_salad = FruitSalad(*fruit_bowl)
+
+    # Assert
+    assert all(fruit.cubed for fruit in fruit_salad.fruit)
 
 def test_add():
     assert add(2,3) == 5
